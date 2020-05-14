35cd0b2bccb204ec7edc08e96e25b3ba981ea37b
oleoot <oleoot28@gmail.com>
2020-05-14 17:45:02 +0300

    Adding neDB database, sending data to it

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
+const Datastore = require('nedb');
 const app = express();
 app.listen(3000, () => console.log('Listening at 3000'))
 app.use(express.static('public'))
 app.use(express.json({ limit: '1mb' }))
 
-const database = []
+const database = new Datastore('database.db');
+database.loadDatabase();
 
 app.post('/api', (request, response) => {
     console.log('I got a request')
     console.log(request.body);
     const data = request.body;
-    database.push(data)
-    console.log(database)
+    database.insert(data)
+    // console.log(database)
     response.json({
         status: 'success',
         latitude: data.lat,
